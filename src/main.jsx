fa2a73f322559ad8cca59d70c5564b6849cd5006
Rasel Hossen <raselhossen052@gmail.com>
2024-02-16 11:34:35 +0600

    router/contacts

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './Root.jsx';
+import ErrorPage from './Error.jsx';
+import Contact from './Contact.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/contacts/:contactId',
+    element: <Contact />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
